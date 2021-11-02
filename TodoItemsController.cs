18aa174c893ac2c3bc006418419dd7b9b798a4ea
yi-hui0928 <yd6185855@gmail.com>
2021-11-03 01:01:56 +0800

    Create TodoItemsController.cs
    上傳API controller 部分的程式碼

---
+++ TodoItemsController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo2.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Todo2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _todoContext;
+        
+        public TodoItemsController(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> Get()
+        {
+            return _todoContext.TodoItems;
+        }
+
+        // GET api/<TodoItemsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> Get (Guid id)
+        {
+            var result = _todoContext.TodoItems.Find(id);
+            if(result == null)
+            {
+                return NotFound("找不到資料");
+            }
+                
+            return result;
+        }
+
+        // POST api/<TodoItemsController>
+        [HttpPost]
+        public ActionResult<TodoItem> Post([FromBody] TodoItem value)
+        {
+            _todoContext.TodoItems.Add(value);
+            _todoContext.SaveChanges();
+            
+
+            return CreatedAtAction(nameof(Get), new { id = value.Id }, value);
+        }
+
+        // PUT api/<TodoItemsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] TodoItem value)
+        {
+            if (id != value.Id)
+            {
+                return BadRequest();
+            }
+
+            _todoContext.Entry(value).State = EntityState.Modified;
+
+            try
+            {
+                _todoContext.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (!_todoContext.TodoItems.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return StatusCode(500, "存取發生錯誤");
+                }
+
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<TodoItemsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var delete = _todoContext.TodoItems.Find(id);
+
+            if(delete == null)
+            {
+                return NotFound("找不到資源");
+            }
+
+            _todoContext.TodoItems.Remove(delete);
+            _todoContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
